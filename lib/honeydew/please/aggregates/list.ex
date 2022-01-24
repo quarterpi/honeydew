2352e257afddcda161f0296b13a5095bf801b3d2
quarterpi <matthew@threefoursoftware.com>
2022-01-24 07:57:20 -0500

    Handle AddList command in aggregate and track status of list.

--- lib/honeydew/please/aggregates/list.ex
+++ lib/honeydew/please/aggregates/list.ex
@@ -6,20 +6,35 @@ defmodule Honeydew.Please.List do
   alias Honeydew.Please.List
   alias Honeydew.Please.Commands.{
     AddList,
+    CompleteList,
   }
   alias Honeydew.Please.Events.{
     ListAdded,
+    ListCompleted,
   }
 
   defstruct [
     :list_id,
     :name,
-    :notes
+    :notes,
+    :status,
   ]
   
-  def execute(%List{}, %AddList{}) do
+  def execute(%List{list_id: nil}, %AddList{list_id: list_id, name: name, notes: notes}) do
+    %ListAdded{
+      list_id: list_id,
+      name: name,
+      notes: notes
+    }
   end
 
-  def apply(%List{}, %ListAdded{}) do
+  def apply(%List{} = list, %ListAdded{} = event) do
+    %List{
+      list
+      | list_id: event.list_id,
+        name: event.name,
+        notes: event.notes,
+        status: :active
+    }
   end
 end
