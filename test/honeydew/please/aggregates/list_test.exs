8f9d9265a02e3528eebfea09b39d8367fad5362e
Chris Martin <chris.martin@oforce.com>
2022-02-13 17:54:56 -0700

    aggregate tests

--- test/honeydew/please/aggregates/list_test.exs
+++ test/honeydew/please/aggregates/list_test.exs
@@ -1,7 +1,21 @@
 defmodule Honeydew.Please.ListTest do
   use ExUnit.Case, async: true
+
   alias Honeydew.Please.List
+  alias Honeydew.Please.Events.ListMade
+  alias Honeydew.Please.Commands.MakeList
 
   use Cqrs.Testing.ExMachina
   use Cqrs.Testing.AggregateCase, aggregate: List
+
+  factory MakeList
+  factory ListMade
+
+  test "can't create a list twice" do
+    list_made = build(:list_made)
+    make_list = build(:make_list)
+
+    assert %{error: {:error, :list_already_exists}} =
+             execute_command([list_made], %{make_list | list_id: list_made.list_id})
+  end
 end
