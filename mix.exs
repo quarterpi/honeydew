3caad9bdcc3487c8703a318ba9c4492e3b606012
Chris Martin <chris.martin@oforce.com>
2022-02-13 17:37:50 -0700

    github deps

--- mix.exs
+++ mix.exs
@@ -60,12 +60,12 @@ defmodule Honeydew.MixProject do
       {:surface_formatter, "~> 0.6.0"},
 
       # cqrs_tools
-      # {:cqrs_tools, github: "elixir-cqrs/cqrs_tools"},
-      # {:cqrs_tools_ddd, github: "elixir-cqrs/cqrs_tools_ddd"},
-      {:cqrs_tools,
-       path: "/Users/chris/code/personal/elixir_cqrs_libs/cqrs_tools", override: true},
-      {:cqrs_tools_ddd,
-       path: "/Users/chris/code/personal/elixir_cqrs_libs/cqrs_tools_ddd", override: true},
+      {:cqrs_tools, github: "elixir-cqrs/cqrs_tools"},
+      {:cqrs_tools_ddd, github: "elixir-cqrs/cqrs_tools_ddd"},
+      # {:cqrs_tools,
+      #  path: "/Users/chris/code/personal/elixir_cqrs_libs/cqrs_tools", override: true},
+      # {:cqrs_tools_ddd,
+      #  path: "/Users/chris/code/personal/elixir_cqrs_libs/cqrs_tools_ddd", override: true},
       {:elixir_uuid, "~> 1.6", override: true, hex: :uuid_utils},
 
       # testing
